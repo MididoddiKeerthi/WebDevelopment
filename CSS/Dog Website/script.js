d090289d9c2fe695a13c7698fc3f560e410d3d46
MididoddiKeerthi <148532996+MididoddiKeerthi@users.noreply.github.com>
2024-09-22 19:50:45 +0530

    Add files via upload

---
+++ CSS/Dog Website/script.js
@@ -0,0 +1,20 @@
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    if (href == "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
